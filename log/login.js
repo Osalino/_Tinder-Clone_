3d3e12ea68a4d79ab9da8990638286dbf6621d5f
Osazemen.odeh@gmail.com <Osazemen.odeh@gmail.com>
2022-11-24 23:59:08 +0000

    Co-authored-by: Ivandro Gaspar <ivandrogaspar@users.noreply.github.com> Co-authored-by: aneestnwr@gmail.com <aneestnwr@gmail.com>

---
+++ log/login.js
@@ -0,0 +1,24 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+//var elem = document.getElementById("register");
+// const login = document.getElementById('login')
+
+function register(){
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+function login(){
+    x.style.left = "0px";
+    y.style.left = "450px";
+    z.style.left = "0px";
+
+}
+
+
+
+// function login() {
+//     document.getElementById(id).style.display = 'block'
+// }
